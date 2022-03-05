bcf58f1fabfc959e80e0e7ddcf2077277be6f9e0
Irelia <irelia@Irelias-MacBook-Pro.local>
2022-03-04 20:23:06 -0500

    testing

--- test/words/test.cpp
+++ test/words/test.cpp
@@ -5,10 +5,23 @@ int main(){
    // Word w;
    // w.getnewword();
     nlohmann::json j;
-    j["category"] = "yo";
-    j["akali"] = nlohmann::json::object();
-    j["akali"]["q"] = "b";
-    std::ofstream o("key.json");
-    o << j;
+    std::ifstream ifs("history.json");
+    if(!ifs.good()){
+        nlohmann::json j ;
+        j["hello"] = nlohmann::json::object();
+        j["hello"]["date"] = 0xdeadbeef;
+        j["hello"]["str"] = "b";
+        std::ofstream ofs("history.json");
+        ofs << j;
+        ofs.close();
+    }else{
+        j = nlohmann::json::parse(ifs);
+        j["dart"] = nlohmann::json::object();
+        j["dart"]["date"] = 0xcafef00d;
+        ifs.close();
+        std::ofstream ofs("history.json");
+        ofs << j;
+        ofs.close();
+    }
     return 0;
 }
