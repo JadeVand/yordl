aa31fb8ca97b630aea08a411cc71371a6d89567d
Irelia <irelia@Irelias-MacBook-Pro.local>
2022-03-07 13:22:40 -0500

    schema

--- test/words/test.cpp
+++ test/words/test.cpp
@@ -2,73 +2,73 @@
 #include <urand.h>
 #include <iostream>
 #include <string.h>
-int uuid_str_valid(const char *uuid)
-{
-    int i, valid;
+char correct[] = "hello";
+class Box{
+public:
+    char c;
+    Box(){
+        c = 0;
+    }
+};
 
-    if (uuid == NULL)
-        return 0;
-
-    for (i = 0, valid = 1; uuid[i] && valid; i++) {
-        switch (i) {
-        case 8: case 13: case 18: case 23:
-            valid = (uuid[i] == '-');
-            break;
-        default:
-            valid = isxdigit(uuid[i]);
-            break;
+#define MAX_BOXES 96 //12x8 max
+class Progress{
+private:
+    Box* b;
+    uint8_t x;
+    uint8_t y;
+    uint8_t index = 0;
+public:
+    Progress(){
+        b = new Box[MAX_BOXES];
+        memset(b,0,sizeof(Box)*MAX_BOXES);
+        x = 5;
+        y = 20;
+        
+    }
+    void push(char* w){
+        typedef Box Box2d[x][y];
+        Box2d* b2 = (Box2d*)b;
+        if(index<5){
+            for(uint8_t i = 0; i < x;++i){
+                (*b2)[index][i].c = w[i];
+            }
+            index++;
         }
+        
     }
-
-    if (i != 36 || !valid)
-        return 0;
-
-    return 1;
-}
-int uuid_str_to_bin(char *uuid_str, unsigned char *uuid_bin, int str_format)
-{
-    uint16_t tmp16;
-    uint32_t tmp32;
-    uint64_t tmp64;
-
-    if (!uuid_str_valid(uuid_str))
-        return -EINVAL;
-
-    if (str_format == UUID_STR_FORMAT_STD) {
-        tmp32 = cpu_to_be32(strtoul(uuid_str, NULL, 16));
-        memcpy(uuid_bin, &tmp32, 4);
-
-        tmp16 = cpu_to_be16(strtoul(uuid_str + 9, NULL, 16));
-        memcpy(uuid_bin + 4, &tmp16, 2);
-
-        tmp16 = cpu_to_be16(strtoul(uuid_str + 14, NULL, 16));
-        memcpy(uuid_bin + 6, &tmp16, 2);
-    } else {
-        tmp32 = cpu_to_le32(strtoul(uuid_str, NULL, 16));
-        memcpy(uuid_bin, &tmp32, 4);
-
-        tmp16 = cpu_to_le16(strtoul(uuid_str + 9, NULL, 16));
-        memcpy(uuid_bin + 4, &tmp16, 2);
-
-        tmp16 = cpu_to_le16(strtoul(uuid_str + 14, NULL, 16));
-        memcpy(uuid_bin + 6, &tmp16, 2);
+    Box* getb(){
+        return b;
     }
-
-    tmp16 = cpu_to_be16(strtoul(uuid_str + 19, NULL, 16));
-    memcpy(uuid_bin + 8, &tmp16, 2);
-
-    tmp64 = cpu_to_be64(strtoul(uuid_str + 24, NULL, 16));
-    memcpy(uuid_bin + 10, (char *)&tmp64 + 2, 6);
-
-    return 0;
-}
+    ~Progress(){
+        delete[] b;
+    }
+};
 int main(){
-    URand ur;
-    char u[37] ={0};
-    uint8_t b[18] = {0};
-    ur.uuid4_generate(u);
-    printf("%s\t%d\n",u,uuid_str_valid(u));
-    if(uuid_str_to_bin(u,b,0)){
-        printf("success\n");
+
+    char input[32] = {0};
+    fgets(input,sizeof(input),stdin);
+    Progress p;
+    p.push(input);
+    fgets(input,sizeof(input),stdin);
+    p.push(input);
+    fgets(input,sizeof(input),stdin);
+    p.push(input);
+    fgets(input,sizeof(input),stdin);
+    p.push(input);
+    fgets(input,sizeof(input),stdin);
+    p.push(input);
+    fgets(input,sizeof(input),stdin);
+    p.push(input);
+    fgets(input,sizeof(input),stdin);
+    p.push(input);
+    fgets(input,sizeof(input),stdin);
+    p.push(input);
+    Box* b = p.getb();
+    typedef Box Box2d[5][20];
+    Box2d* b2 = (Box2d*)b;
+    for(int i = 0 ; i < 20; ++i){
+        printf("%s\n",(*b2)[i]);
     }
+    return 0;
 }
